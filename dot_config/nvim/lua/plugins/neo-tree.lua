498642162cfbf3d56c6e7b8e9501b779067585d0
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2024-06-19 22:31:45 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/theme.lua

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle=true<CR>", {})
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
