41b94ee8567a3934c94838ceba01af2b38d925aa
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-03-29 17:28:02 +0100

    Update init.sh

--- executable_init.sh
+++ executable_init.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 dnf copr enable atim/lazygit -y
-dnf install vim nvim zsh eza bat lazygit tmux
+dnf install vim nvim zsh eza bat lazygit tmux https://github.com/derailed/k9s/releases/download/v0.40.10/k9s_linux_amd64.rpm
 sh -s $(which zsh)
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 curl -sS https://starship.rs/install.sh | sh
 
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:prokobit/dotfiles.git
-
