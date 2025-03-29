5a654b52b5c5f71f34fa71a60884c0e7fd68cb59
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-03-29 16:52:20 +0100

    Add init.sh

--- executable_init.sh
+++ executable_init.sh
@@ -1,8 +1,9 @@
-```bash init.sh
 #! /bin/bash
-dnf install vim nvim zsh eza bat tmux
+dnf copr enable atim/lazygit -y
+dnf install vim nvim zsh eza bat lazygit tmux
 sh -s $(which zsh)
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 curl -sS https://starship.rs/install.sh | sh
+
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:prokobit/dotfiles.git
-```
+
