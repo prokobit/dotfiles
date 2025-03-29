a0520576958417184097c4fe09e5643c0a61cfdc
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-03-29 17:42:00 +0100

    Update init.sh

--- executable_init.sh
+++ executable_init.sh
@@ -5,10 +5,14 @@ sh -s $(which zsh)
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 curl -sS https://starship.rs/install.sh | sh
 
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:prokobit/dotfiles.git
+
 mkdir -p "$(bat --config-dir)/themes"
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
 bat cache --build
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:prokobit/dotfiles.git
+mkdir -p "$HOME/workspace"
+#cd workspace $$ git clone git@github.com:sharkdp/vivid.git
+#cd vivid cargo install vivid
