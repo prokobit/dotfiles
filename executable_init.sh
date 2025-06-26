21b851f517e4975299c43848a88790b6afb4998d
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-06-26 23:18:39 +0200

    Update init.sh

--- executable_init.sh
+++ executable_init.sh
@@ -3,11 +3,11 @@ dnf install vim nvim zsh eza bat fzf tmux pipx uv libheif-freeworld
 
 sh -s $(which zsh)
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-curl -sS https://starship.rs/install.sh | sh
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:prokobit/dotfiles.git
 curl https://mise.run | sh
 mise install
+
+chezmoi init git@github.com:prokobit/dotfiles.git
 
 mkdir -p "$(bat --config-dir)/themes"
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
