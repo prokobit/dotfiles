1678f22079048e17e9926a3f41b3e5f1c254bf61
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2025-10-05 12:53:39 +0200

    Add .config/nvim/lua/plugins/sealed-secrets.lua

---
+++ dot_config/nvim/lua/plugins/sealed-secrets.lua
@@ -0,0 +1,11 @@
+
+return {
+  "tinoschroeter/sealed-secrets.nvim",
+  -- dir = "~/projects/sealed-secrets.nvim", -- dev only
+  config = function()
+    require("sealed-secrets").setup({
+      -- the path can also be relative ../../sealedsecrets.pem
+      cert_path = "../../../../.kube/k8s.pem"
+    })
+  end,
+}
